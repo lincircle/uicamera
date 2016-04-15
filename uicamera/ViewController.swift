e1712b13ec22975384b917d23f9c90960b6994ce
lincircle <cosine@cgua.local>
2016-04-15 21:05:10 +0800

    Initial Commit

---
+++ uicamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uicamera
+//
+//  Created by cosine on 2016/4/15.
+//  Copyright © 2016年 Lin Circle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
